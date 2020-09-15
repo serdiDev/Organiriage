27a17a641f82686e8aa9739bf6b3a9365e3e75b3
Servais DD NKOUKA <servaisnkouka6@gmail.com>
2020-09-15 14:09:01 +0100

    Get author image from cloudinary

--- components/author.js
+++ components/author.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import {Image, CloudinaryContext} from 'cloudinary-react'
 
 export default function author() {
     return (
         <div className="flex w-full items-center font-sans p-8 md:p-24">
-            <img className="w-10 h-10 rounded-full mr-4" src="/images/lion.png" alt="Avatar of Author" />
+            <CloudinaryContext cloudName="serdidev" >
+                <Image publicId="lion_v8oyrx" className="w-10 h-10 rounded-full mr-4" alt="Avatar of Author" />
+            </CloudinaryContext>
             <div className="flex-1">
-                <p className="text-base font-bold text-base md:text-xl leading-none">Organiriage</p>
+                <p className="font-bold text-base md:text-xl leading-none">Organiriage</p>
                 <p className="text-gray-600 text-xs md:text-base">
                     Un site vitrine d'une agence d'organistion de mariage par 
                     <a className="text-gray-800 hover:text-indigo-500 no-underline border-b-2 border-indigo-500" href="https://github.com/serdiDev"> SerDiDev</a></p>
